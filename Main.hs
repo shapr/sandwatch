6319de3ee33478fdf90fb96cb290a9b5bcba7b69
Shae Erisson <shae@scannedinavian.com>
2020-05-25 17:15:37 -0400

    fix sandwich formatting, need sandwich now

--- Main.hs
+++ Main.hs
@@ -5,9 +5,16 @@ import           Criterion.Measurement.Types (Measured (..), nfAppIO)
 import qualified Data.Text.Lazy              as T
 import           Data.Time                   (getCurrentTime)
 import           MyLib
+import           Numeric
 import           System.Directory            (getCurrentDirectory)
 import           System.Environment
 import           System.Process
+-- | A sandwich takes five minutes according to my poll: https://twitter.com/shapr/status/1263950892744806402
+sandwichSeconds = 5 * 60
+
+-- | How many words at the beginning of the command line to match?
+wordsToMatch :: Int
+wordsToMatch = 3
 
 main :: IO ()
 main = do
@@ -16,8 +23,8 @@ main = do
   now <- getCurrentTime
   thiscwd <- getCurrentDirectory
   let thisRun = Run now (T.strip . T.pack $ unwords args) (T.pack thiscwd) 0 0 0 -- could I treat cmdline params as a Set?
-  let guess = guessTime 2 history thisRun
-  putStrLn $ "This command will probably take " <> show (guess / 5 * 60) <> " sandwiches to complete."
+  let guess = guessTime wordsToMatch history thisRun
+  putStrLn $ "This command will probably take " <> (formatFloat  (guess / (5 * 60))) <> " sandwiches to complete."
   let bmark = nfAppIO callCommand (unwords args)
   (msr,_) <- measure bmark 1
   let thisRun' = thisRun { runTime = measTime msr
@@ -25,4 +32,5 @@ main = do
                          , cpuCycles = measCycles msr
                          }
   writeSandWatchData $ Runs (thisRun' : (rs history))
-  print $ measTime msr
+  putStrLn $ "Actual time was " <> (formatFloat $ (measTime msr) / sandwichSeconds) <> " sandwiches."
+      where formatFloat f = showFFloat (Just 2) f ""
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # sandwatch
-Remember how long commands take, tell me if I have enough time to make a sandwich
+Remember how long commands take, tell me if I have enough time to make a sandwich?
+
+This is inspired by [arbtt](https://arbtt.nomeata.de/#what), the shell built-in time, and the rust utility [tally](https://crates.io/crates/tally).
+
+The name was suggested by [qu1j0t3](https://github.com/qu1j0t3/).
+
+# Why?
+
+Rather than telling me how long something has taken, I would like to know roughly how long this task will take, based on historical records.
+
+Sandwatch records the current directory, the command line, and the runtime for a command.
+
+When running a new command that matches the directory and the first two words of the command line, sandwatch reports expected completion time in sandwich units (five minutes?).
